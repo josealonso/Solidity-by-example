1a0c81280d7950112144490e8f256bd9b2b90b04
josealonso <jose.alonso.programmer@gmail.com>
2021-09-15 14:19:07 +0200

    Using conditional statements if, else if and else.

---
+++ IfElse.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract IfElse {
+    function foo(uint x) public pure returns (uint) {
+        if (x < 10) {
+            return 0;
+        } else if (x < 20) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
+
